2593220d30a80eb368c58d55be78c2fe31468038
Gustavo Souza Gonçalves <gustavosouzagoncalves@gmail.com>
2013-02-17 19:46:20 -0300

    r

---
+++ UserControls/GraphChart.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UserControls
+{
+    public sealed partial class GraphChart : UserControl
+    {
+        public GraphChart()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new GraphChartViewModel();
+        }
+    }
+}
---
+++ UserControls/GraphChartViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace UserControls
+{
+    public class GraphChartViewModel : INotifyPropertyChanged
+    {
+
+        public GraphChartViewModel()
+        {
+            Titulo = "Titulo Teste";
+        }
+
+        ~GraphChartViewModel()
+        {
+
+        }
+
+        private String titulo;
+        public String Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                titulo = value;
+                OnPropertyChanged("Titulo");
+            }
+        }
+
+        public void OnPropertyChanged(String property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
